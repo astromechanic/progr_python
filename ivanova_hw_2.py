ad9c590b57c48185780ed65c744e36b9e9805385
astromechanic <violett.ivanova@gmail.com>
2016-10-08 17:49:38 +0400

    Add files via upload

---
+++ ivanova_hw_2.py
@@ -0,0 +1,43 @@
+import urllib.request
+import re
+
+def download(commonUrl):
+
+    req = urllib.request.Request(commonUrl)     
+    with urllib.request.urlopen(req) as response:
+        html = response.read().decode('windows-1251')
+
+    regPostTitle = re.compile('mnname.*?html', flags=re.U | re.DOTALL)
+    all_links = regPostTitle.findall(html)
+
+    new_links = []
+    regex = 'mnname"><a href="/'
+    basicUrl = 'http://znamyatrud.ru/'
+    for link in all_links:
+        link = re.sub(regex, '', link)
+        link_1 = basicUrl + link
+        new_links.append(link_1)      
+
+    f = open('links.txt', 'a')
+    for t in new_links:
+        f.write(t + '\n')
+
+    f.close()
+    return 0
+
+
+
+
+def main():
+
+    commonUrl = 'http://znamyatrud.ru/news.html?page='
+    for i in range(1, 5):
+        s = commonUrl + str(i)
+        download(s)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
